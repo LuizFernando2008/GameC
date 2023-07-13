918060ae9ec2e487311e1382129872fe9e6fe5aa
Luiz Fernando <luizfernandooliveira360@gmail.com>
2023-07-13 13:02:24 -0300

    game

---
+++ game.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int dificuldade;
+	int valor1;
+	int valor2;
+	int operacao;
+	int resultado;
+} Calcular;
+
+void jogar();
+void mostrarInfo(Calcular calc);
+int somar(int resposta, Calcular calc);
+int subtrair(int resposta, Calcular calc);
+int multiplicar(int resposta, Calcular calc);
+
+int pontos = 0;
+
+int main(){
+
+	srand(time(NULL));
+
+	jogar();
+
+	return 0;
+}
+
+void jogar(){
+	Calcular calc;
+	int dificuldade;
+
+	printf("Informe o nível de dificuldade desejado[1, 2, 3, 4]:\n");
+	fflush(stdout);
+	scanf("%d", &dificuldade);
+	calc.dificuldade = dificuldade;
+
+	calc.operacao = rand() % 3;
+
+	if(calc.dificuldade == 1){
+		calc.valor1 = rand() % 11;
+		calc.valor2 = rand() % 11;
+	}else if(calc.dificuldade == 2){
+		calc.valor1 = rand() % 101;
+		calc.valor2 = rand() % 101;
+	}else if(calc.dificuldade == 3){
+		calc.valor1 = rand() % 1001;
+		calc.valor2 = rand() % 1001;
+	}else if(calc.dificuldade == 4){
+		calc.valor1 = rand() % 10001;
+		calc.valor2 = rand() % 10001;
+	}else{
+		calc.valor1 = rand() % 100001;
+		calc.valor2 = rand() % 100001;
+	}
+	int resposta;
+	printf("informe o resulado para a seguinte operação: \n");
+
+	if(calc.operacao == 0){
+		printf("%d + %d\n", calc.valor1, calc.valor2);
+		fflush(stdout);
+		scanf("%d", &resposta);
+
+		if(somar(resposta, calc)){
+			pontos += 1;
+			printf("Você tem %d pontos(s).\n", pontos);
+		}
+	}else if(calc.operacao == 1){
+		printf("%d - %d\n", calc.valor1, calc.valor2);
+		fflush(stdout);
+		scanf("%d", &resposta);
+
+		if(subtrair(resposta, calc)){
+			pontos += 1;
+			printf("Você tem %d pontos(s).\n", pontos);
+		}
+	}else if(calc.operacao == 2){
+		printf("%d x %d\n", calc.valor1, calc.valor2);
+		fflush(stdout);
+		scanf("%d", &resposta);
+
+		if(multiplicar(resposta, calc)){
+			pontos += 1;
+			printf("Voçê tem %d ponto(s).\n", pontos);
+		}
+	}else{
+		printf("A operação %d não é reconhecida.\n", calc.operacao);
+	}
+
+//	mostrarInfo(calc);
+
+	printf("Deseja continuar jogando? [1 - sim, 0 - não]\n");
+	int continuar;
+	fflush(stdout);
+	scanf("%d", &continuar);
+
+	if(continuar){
+		jogar();
+	}else{
+		printf("Você finalizou com %d ponto(s).\n", pontos);
+		printf("Até a próxima!\n");
+		exit(0);
+	}
+}
+
+void mostrarInfo(Calcular calc){
+	char op[25];
+
+	if(calc.operacao == 0){
+		sprintf(op, "Somar");
+	}else if(calc.operacao == 1){
+		sprintf(op, "Subtração");
+	}else if(calc.operacao == 2){
+		sprintf(op, "Multiplicar");
+	}else{
+		sprintf(op, "Operação desconhecida.");
+	}
+	printf("Valor 1: %d \nValor 2: %d \nDificuldade: %d \nOperação: %s\n",
+			calc.valor1, calc.valor2, calc.dificuldade, op);
+}
+
+int somar(int resposta, Calcular calc){
+	int resultado = calc.valor1 + calc.valor2;
+	calc.resultado = resultado;
+	int certo = 0;
+
+	if(resposta == calc.resultado){
+		printf("Resposta correta!\n");
+		certo = 1;
+	}else{
+		printf("Resposta errada.\n");
+	}
+	printf("%d + %d = %d\n", calc.valor1, calc.valor2, calc.resultado);
+
+	return certo;
+}
+
+int subtrair(int resposta, Calcular calc){
+	int resultado = calc.valor1 - calc.valor2;
+	calc.resultado = resultado;
+	int certo = 0;
+
+	if(resposta == calc.resultado){
+		printf("Resposta correta!\n");
+		certo = 1;
+	}else{
+		printf("Resposta errada.\n");
+	}
+	printf("%d - %d = %d\n", calc.valor1, calc.valor2, calc.resultado);
+
+	return certo;
+}
+
+int multiplicar(int resposta, Calcular calc){
+	int resultado = calc.valor1 * calc.valor2;
+	calc.resultado = resultado;
+	int certo = 0;
+
+	if(resposta == calc.resultado){
+		printf("Resposta correta!\n");
+		certo = 1;
+	}else{
+		printf("Resposta errada.\n");
+	}
+	printf("%d x %d = %d\n", calc.valor1, calc.valor2, calc.resultado);
+
+	return certo;
+}
